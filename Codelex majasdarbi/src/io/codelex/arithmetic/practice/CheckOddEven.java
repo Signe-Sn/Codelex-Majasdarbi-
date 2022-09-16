bce915685f1448d99a0c21c6f73fc8103eb0576f
Signe-Sn <signe.snt@gmail.com>
2022-09-16 14:59:06 +0300

    izlaboti majasdarbi

--- Codelex majasdarbi/src/io/codelex/arithmetic/practice/CheckOddEven.java
+++ Codelex majasdarbi/src/io/codelex/arithmetic/practice/CheckOddEven.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class CheckOddEven {
     public static void main(String[] args) {
 
-        int num;
-
         Scanner exercise2 = new Scanner(System.in);
-        System.out.println("Please enter a number: ");
-        num = exercise2.nextInt();
+        System.out.print("Please enter a number: ");
+        int num = exercise2.nextInt();
 
         if (num % 2 == 0){
             System.out.println("Even Number");
-            System.out.println("Bye!");
         } else {
             System.out.println("Odd Number");
-            System.out.println("Bye!");
         }
+        System.out.println("Bye!");
     }
 }
--- Codelex majasdarbi/src/io/codelex/arithmetic/practice/Product1ToN.java
+++ Codelex majasdarbi/src/io/codelex/arithmetic/practice/Product1ToN.java
@@ -5,9 +5,9 @@ public class Product1ToN {
         int num1 = 1;
         int num2 = 10;
 
-    for(int i = num1; i <= num2; i++){
-        num1 *= i;
-    }
+        for (int i = num1; i <= num2; i++) {
+            num1 *= i;
+        }
         System.out.println(num1);
     }
 }
